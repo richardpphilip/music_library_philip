c8d4bbf8aa63dcb07977c2ab22c6f5ae2d222a28
richardpphilip <richardpphilip1@gmail.com>
2021-04-15 13:51:53 -0700

    added the get by id, the put, and the delete method.  tested and the program works as intended.

--- music_project/music/urls.py
+++ music_project/music/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('music/', views.SongList.as_view())
+    path('music/', views.SongList.as_view()),
+    path('music/<int:pk>/', views.SongDetail.as_view())
 ]
