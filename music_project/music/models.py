d212c7b53397e1921c8be9ce30a69d84110d48de
richardpphilip <richardpphilip1@gmail.com>
2021-04-15 10:58:47 -0700

    created django framework and registered Song App

---
+++ music_project/music/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=50, default=None)
+    artist = models.CharField(max_length=50, default=None)
+    album = models.CharField(max_length=50, default=None)
+    release_date = models.DateField(null=True, blank=False)
